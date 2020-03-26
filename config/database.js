9129fe05d63939a02ffe7859e1273a32516e87ce
Ummimath Farisha <farishaummimath@gmail.com>
2020-03-26 23:05:43 +0530

    updated db config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
+// mongodb+srv://myMongoUser:<password>@cluster0-pqhed.mongodb.net/test?retryWrites=true&w=majority
+
 const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mern-app'
 const setupDB = ()=>{
     mongoose.connect(CONNECTION_URI,{useNewUrlParser:true, useUnifiedTopology: true })
